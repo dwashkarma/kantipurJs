53737f11cb6282f7486c57611c8b123b28243cfd
DwashKarma <dwaskc@gmail.com>
2024-02-18 09:01:10 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// console.log("Hello World");
+
+//Data Type
+// ===============================
+// 1.Stirngs => ""
+// 2.Number => 0/1/2/3
+// 3.Boolean => true / false
+// 4.Null => null
+// 5.Undefined => undefined
+// 6.Objects => { }
+// 7. Arrays => [ ]
+
+//variable Types
+//1. let
+//2. var
+//3.  const
+
+// let name = "Dwash"; //string
+// var address = "kathmandu"; //string
+// const post = "frontend"; //string
+// console.log(name);
+// console.log(address);
+// console.log(post);
+
+//Number
+// ===========================
+
+// let num1 = 10; //Number
+// let num2 = 30; //Number
+// console.log(num1);
+// console.log(num2);
+// console.log(num1 + num2);
+
+//Boolean
+
+// let boolean1=false
+// let boolean2=true
+// console.log(boolean1)
+// console.log(boolean2)
+
+//Object
+
+let object = {
+  name: "Dwash",
+  address: "ktm",
+  contact: 98000000,
+  boolean: true,
+  post: "frontend",
+  Null: null,
+  job: undefined,
+};
+
+console.log(object);
